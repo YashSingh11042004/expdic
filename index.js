2190dec07556ac0371ca46fa26a367db2c03aee0
Yash Singh <154120524+YashSingh11042004@users.noreply.github.com>
2025-03-12 12:30:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
  
+const express = require('express');
+const app = express();
+
+let port = 3000;
+
+app.listen(port, () =>
+   console.log(`Server running on ${port}`));
+
+app.use((req, res) => {
+  console.log(req);
+  console.log("✅ Node is running!");
+});
+
